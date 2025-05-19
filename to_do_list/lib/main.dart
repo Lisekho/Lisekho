de34537a99e6e855c9185a52191d7b5fbaf3a6b9
Siyakholwa Lisekho Tototo <102138370+Lisekho@users.noreply.github.com>
2025-05-19 18:56:46 +0200

    More code
    Building in More Widgets

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('Hello World ')));
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("To-do list"), centerTitle: true),
+      body: Center(child: Text("Tasks of the day")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: AbsorbPointer.new,
+        child: Text('click'),
+      ),
+    ),
+  ),
+);
